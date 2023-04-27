86c04f55d4fbadcecfca931c334b9d08c147f3f6
Sri HariHaran R <76623387+Sri-HariHaran-R@users.noreply.github.com>
2023-04-27 23:26:22 +0530

    Update linkedlist.java

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -168,7 +168,7 @@ class backend
             while(fast.next!=null && fast.next.next!=null)
             {
                 fast=fast.next.next;
-                slow=slow.next.next;
+                slow=slow.next;
                 
                 if(fast==slow)
                     return true;
